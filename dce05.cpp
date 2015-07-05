ec4a506f72baf4353ec5c6b219074cb7899d86f4
nickzuck <nikhil1945.singh@gmail.com>
2015-07-05 12:37:26 +0530

    Accepted solution

--- dce05.cpp
+++ dce05.cpp
@@ -1,24 +1,23 @@
-#include<iostream> 
+#include<cstdio> 
+#include<cmath>
 
-using namespace std ; 
+//using namespace std ; 
 
 int main ()
 {
 
-	long long int div , t ,  n  ;  
-	cin >> t ; 
+	long long int  t ,  n , ans  ;  
+	scanf ("%lld",&t) ; 
 	while (t--){
-		cin >> n ;  
+		scanf ("%lld",&n) ;  
 		int flag = 0 ;
-		div = 1073741824    ;  
-		while (flag == 0 ){
-			if (n / div != 0)	
-				flag = 1 ;  
-			else {
-				div /= 2 ; 
-			}
+		//div = 1073741824    ;  
+		while (n!=0 ){
+			n /= 2 ; 
+			flag ++ ;  
 		}
-		cout << div <<endl ;
+		ans = pow (2 , flag-1 ); 
+		printf ("%lld\n",ans) ; 
 		
 	}
 return 0 ; 
