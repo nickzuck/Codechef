8f99c0d0fbede221d6ffea606af6791aec7f1319
nickzuck <nikhil1945.singh@gmail.com>
2015-07-12 20:13:06 +0530

    Trying to get the 8 points ..lack of time

---
+++ lckyst.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std ; 
+
+int main ()
+{
+	int n , a , i ; 
+	for (i = 0 ; i<n ; i++)	
+	{
+		cin >>  a; 
+		if (a == 5)
+			cout << 20 << endl ; 
+		else if (a == 10 )
+			cout << 40 << endl ;
+		else 
+			cout << a << endl ; 
+	}
+return 0 ; 
+}
