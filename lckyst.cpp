5a558d4a58007b12301af2e93830cd1682436e1b
nickzuck <nikhil1945.singh@gmail.com>
2015-07-12 21:04:49 +0530

    GOT 8 POINTS ...Tried to complete the whole question ..but there was a bug...so optinG to complete just next subtask

--- lckyst.cpp
+++ lckyst.cpp
@@ -5,15 +5,38 @@ using namespace std ;
 
 int main ()
 {
-	int n , a , i ; 
+	long long int n , a , i ,j ,  temp , zeros , flag , ans , init; 
 	cin >> n ; 
 	for (i = 0 ; i<n ; i++)	
 	{
-		cin >>  a; 
-		if (a == 5)
-			cout << 20 << endl ; 
-		else 
+		cin >>  a;
+		temp = a ;  
+		zeros = 0 ;
+		flag = 1 ;  
+		while (flag == 1){
+			if (temp %5 ==0){
+				zeros ++ ; 
+				temp /= 5 ; 
+			}
+			else 
+				flag = 0 ; 
+		}
+		if (zeros == 0 )
 			cout << a << endl ; 
+		else {
+			init = 10 ; 
+			for (j = 2 ; j<=zeros ; j++){init *= 10 ; }
+			//cout << init << endl; 
+			ans = 0 ; 
+			for (int k = 2 ; k <= 9 && ans == 0 ; k++){
+				if (init % a == 0 )
+					ans = init ; 
+				else 
+					init = init * k ; 	
+			}
+			cout << ans << endl ; 
+		}
+		
 	}
 return 0 ; 
 }
