ad9398763b86a5d1a3369addc14a2dce5d53240a
nickzuck <nikhil1945.singh@gmail.com>
2015-07-12 22:09:26 +0530

    Got 30 points for this solution

--- lckyst.cpp
+++ lckyst.cpp
@@ -3,9 +3,11 @@
 
 using namespace std ; 
 
+long long int four (long long int ) ; 
+
 int main ()
 {
-	long long int n , a , i ,j ,  temp , zeros , flag , ans , init; 
+	long long int n , a , i ,temp , zeros , flag ; 
 	cin >> n ; 
 	for (i = 0 ; i<n ; i++)	
 	{
@@ -21,22 +23,37 @@ int main ()
 			else 
 				flag = 0 ; 
 		}
+		temp = a ; 
+		long long int twos = 0 ; 
+		flag = 1 ;  
+		while (flag == 1){
+			if (temp %2 ==0){
+				twos ++ ; 
+				temp /= 2 ; 
+			}
+			else 
+				flag = 0 ; 
+		}
+		//cout << "Zeros = "<< zeros << endl; 
+		if (zeros > twos )
+				zeros = zeros - twos ;
+			else
+				zeros  = 0 ; 
 		if (zeros == 0 )
 			cout << a << endl ; 
 		else {
-			init = 10 ; 
-			for (j = 2 ; j<=zeros ; j++){init *= 10 ; }
-			//cout << init << endl; 
-			ans = 0 ; 
-			for (int k = 2 ; k <= 9 && ans == 0 ; k++){
-				if (init % a == 0 )
-					ans = init ; 
-				else 
-					init = init * k ; 	
-			}
-			cout << ans << endl ; 
+			temp = four (zeros) ; 
+			cout << a * pow (4,temp)<<endl ; 
 		}
 		
 	}
 return 0 ; 
+}
+
+long long int four (long long int zeros )
+{
+	if (zeros % 2 == 0 )
+		return  zeros /2 ; 
+	else 
+		return (zeros+1)/2 ; 
 }
