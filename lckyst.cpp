ad185cfdf8d31510a89e103748706fdc59351686
nickzuck <nikhil1945.singh@gmail.com>
2015-07-12 20:24:29 +0530

    Still not got those 8 points .trying again

--- lckyst.cpp
+++ lckyst.cpp
@@ -6,13 +6,12 @@ using namespace std ;
 int main ()
 {
 	int n , a , i ; 
+	cin >> n ; 
 	for (i = 0 ; i<n ; i++)	
 	{
 		cin >>  a; 
 		if (a == 5)
 			cout << 20 << endl ; 
-		else if (a == 10 )
-			cout << 40 << endl ;
 		else 
 			cout << a << endl ; 
 	}
