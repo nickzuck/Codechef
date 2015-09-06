684f271a80b604040f478d04a2a9d51d0a7b70fd
nickzuck <nikhil1945.singh@gmail.com>
2015-09-06 11:36:16 +0530

    Still getting the same damm (AC ,WA ,TLE) combination

--- donuts.cpp
+++ donuts.cpp
@@ -23,32 +23,30 @@ int main ()
                 ones++ ; 
             }
         } 
-        
+        long long int others  = m-ones ; 
         // there is not even a single chain
         if (n == m){ 
             cout << n / 2 << endl ; 
         }
 
-        //all the donuts are in smaller chains
-        else if (ones == 0) {
-            cout << m-1 << endl ; 
-        }
-
-        // some donuts are in chain a some are independent 
-        else {
-            
-           long long int non_ones = m - ones; 
-           if (ones == non_ones-1){
-                cout << ones << endl ;  // only the single donuts are needed to be broken
-            } 
+        else
+        {
+            if (ones > others){
+                long long int s1 =  others-1 ; 
+                long long int s2 =  ones - s1 ; 
+                if (s2 % 2 == 0){
+                    s2 = s2/2 ; 
+                }
+                else {
+                    s2 = s2/2+1 ; 
+                }
+            cout << s1+s2 << endl ; 
+            }
 
             else {
-                long long int s1 = non_ones -1 ; 
-                long long int s2 = ones/2 ; 
-                cout << s1 + s2 << endl ; 
+                cout << m-1-ones << endl ; 
             }
         }
-
         
     }    
 return 0 ;
