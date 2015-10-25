d5fe2dbc8e5fc28a181f42a236042f2564c7e48b
nickzuck <nikhil1945.singh@gmail.com>
2015-10-25 23:56:51 +0530

    don't know why I am adding this file again

---
+++ wdtbam.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+
+#define MAX 1003  
+
+using namespace std ; 
+
+int main ()
+{
+	long long int t , n , w[MAX], ans , i , j , k , len ; 
+
+	char correct[MAX] , chef[MAX] ;
+	cin >> t ;
+	while (t--){
+		cin >> n  ; 
+		for (int i = 0 ; i <n ; i++){
+			cin >> correct[i] ; 
+		}	
+		len = 0 ;
+		for (j = 0 ; j<n ; j++){
+			cin >> chef[j] ; 
+			if (chef[j] == correct[j]){
+				len ++ ; 
+			}
+		}
+	
+		for (k= 0 ; k<= n ;k++){
+			cin >> w[k] ; 
+		}
+
+
+		if (len != n){	
+			ans = 0; 
+			for (k =0 ; k<=len ; k++){
+				if (ans < w[k])
+					ans = w[k] ; 
+			}
+			cout << ans << endl ;
+		}
+		
+		else {
+			cout << w[n] << endl  ;
+		}
+
+		
+	}
+
+return 0 ;	
+}
