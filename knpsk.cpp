e151b5082df116dc5a29cc721827fce314bcce93
nickzuck <nikhil1945.singh@gmail.com>
2016-01-10 00:41:18 +0530

    WA..Need to set jLimit

---
+++ knpsk.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<algorithm>
+
+#define MAX 100002
+
+using namespace std; 
+
+int main(){
+    long long int t , i , j , a1[MAX] , a2[MAX], n , a , b, ans, k , w; 
+    cin >> t  ; 
+    i = 0 ; j = 0 ; w = 0;
+    while(t--){
+        cin >> a >> b ;
+        w += a;
+        if(a== 1){
+            a1[i++] = b ;
+        }
+        else{
+            a2[j++] = b ; 
+        }
+    }
+
+        cout <<"i = " << i << endl ;
+        cout <<"j = " << j << endl ;
+        sort(a1, a1+i);
+        reverse(a1, a1+i);
+        sort(a2, a2+j) ; 
+        reverse(a2, a2+j); 
+
+        cout << "w = " << w << endl ;
+        long long int iLimit = i ;
+        for(k=1 ; k<=w ; k++){
+            i = 0 ; j= 0 ; ans = 0; 
+            n = k ;
+            while(n>0){
+                if(n==1){
+                    ans += a1[i++] ; 
+                }
+                else{
+                    // Maximum of a2[j] and a1[i] + a1[i+1]
+                    if(a2[j] > a1[i] + a1[i+1] && i+1 <= iLimit){
+                        ans += a2[j];
+                        j ++ ;    
+                    }
+                    else{
+                        ans += a1[i] + a1[i+1] ; 
+                        i += 2;
+                    }
+
+                }
+                n -= 2;
+            }
+            cout << ans << " ";
+
+        }
+        cout << endl; 
+
+return 0 ;
+}
