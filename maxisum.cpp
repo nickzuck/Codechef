947beacf8fcf1b5d73c329972d11894dec2cd205
nickzuck <nikhil1945.singh@gmail.com>
2016-03-08 12:57:34 +0530

    Changing idea again

--- maxisum.cpp
+++ maxisum.cpp
@@ -1,5 +1,24 @@
-/* Idea: Calculate the sum of all positive and negative multiplications from 0 to n-1
+/*
+IDEA: Find the min value of B[i]  corresponding to -ve A[i]
+      Find the max value of B[i]  corresponding to +ve A[i]
+      And increase or decrease the value according to the values of maxb and minb which can determine the change which  must be followed to make the effect better
+ */
 
-   If the abs(-veSum) > abs(+veSum) ; then reduce the negative values. For this keep a array which denotes the indices at which the multiplication results in -ve numbers. Try to make the values of those indices as 0. If we make all the -ve values to zero and are still left with some operations, then increase the value of maximum positive multiplication. If we have less operations for making all the values as 0, then try to reduce the effect of most negative values. 
- 
-  */
+#include<iostream>
+
+using namespace std ; 
+
+def findMinMaxB(long long a[], long long b[], long long n){
+    long long mini, maxi, i; 
+    maxb = -100002 ; 
+    minb = 100002 ; 
+    for(i = 0 ; i< n; i ++){
+        if(a[i] > 0){
+            //find maximum b
+            if(maxb < b[i]){
+                
+            }
+        }
+    }
+
+}
