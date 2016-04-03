13f3adacca7f53eeac30d8a9603127fa81055f93
nickzuck <nikhil1945.singh@gmail.com>
2016-04-03 15:04:32 +0530

    Still getting TLE ...Moving to c++

--- bipin3.py
+++ bipin3.py
@@ -1,7 +1,21 @@
+def ipow(base, exp):
+    result = 1
+    while(exp):
+        if(exp&1):
+            result *= base
+        exp >>= 1
+        base *= base
+    return result
+
+
 for t in range(input()):
     n, k = map(int , raw_input().split())
     if k ==1 and n== 1:
     	print "1"
     else:
-        ans = ((k-1)**(n-1))*k
+        # Inefficient method
+        #ans = ((k-1)**(n-1))*k
+
+        # Efficient method
+        ans = (ipow(k-1, n-1))*k
         print ans%1000000007
