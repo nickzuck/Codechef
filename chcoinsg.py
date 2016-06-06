d2f3ae72a8bfdd104af58e5fa53b53c98590d3fb
nickzuck <nikhil1945.singh@gmail.com>
2016-06-06 12:40:14 +0530

    Finally AC

--- chcoinsg.py
+++ chcoinsg.py
@@ -1,8 +1,7 @@
-# Checking what I thought till now for 10 is correct or not
 t = input()
 for _ in range(t):
     n = input()
-    if n==6:
+    if n%6 == 0:
         print "Misha"
     else:
         print "Chef"
