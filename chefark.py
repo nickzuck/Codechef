5ba92a37aa0cf4d3313f530bae1a7d61b9d5504a
nickzuck <nikhil1945.singh@gmail.com>
2016-06-11 12:41:23 +0530

    WA ... AC only for task 0 and 2

---
+++ chefark.py
@@ -0,0 +1,30 @@
+# just to check what I thought is correct or not
+# Trying to solve subtask 1 
+
+from math import factorial as f 
+def binomial(n, k):
+    if n == k:
+        return 1
+    a = f(n)
+    b = f(n-k)
+    c = f(k)
+    div = a // (b*c)
+    return div
+
+
+for _ in range(input()):
+    n , k = map(int, raw_input().split())
+    arr = map(int, raw_input().split())
+    if k>n : 
+        k = k%n 
+    ans = 0 
+    if k%2 == 0 :
+        if n == 2 and k ==2 : 
+            k  -= 1
+        ans =1 
+        for i in range(2,k+1,2):
+            ans += binomial(n,i)
+    else:
+        for i in range(1,k+1, 2):
+            ans += binomial(n,i)
+    print ans
