178938215c2169f0c6eecfaf04c4c66f1089a4ba
nickzuck <nikhil1945.singh@gmail.com>
2016-06-11 19:15:23 +0530

    Finding the powers correctly

--- chearmy.py
+++ chearmy.py
@@ -1,17 +1,24 @@
+def abc(k):
+    count = 0
+    s = 2
+    temp = k/5
+    while(temp > 0):
+        temp /= 5
+        count += 1 
+        s *= 10
+    print "s = ", s
+    print "count = ", count
+
+for _ in range(input()):
+    abc(input())
+"""
 for _ in range(input()):
     k = input()
+    k = k-1
     arr= [0, 2, 4, 6]
-    t1 = k%25 
-    if t1 == 0:
-        ans = 200*((k-1)/25)
-    else:
-        ans = 200*(k/25)
-    k = t1
-    temp = k%5 
-    if temp == 0 and k != 0:
-        ans += ((k/5) -1) * 20 + 8
-    elif k == 0:
-        ans += 88
-    else:
-        ans += (k/5)*20 + (temp-1)*2
-    print ans
+    count = 0 
+    temp = k
+    while(k!=0):
+        k = k/5 
+        count += 1
+""" 
