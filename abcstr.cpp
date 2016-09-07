9fcce176a93a17e02e76b3249bac5b9dd031a429
nickzuck <nikhil1945.singh@gmail.com>
2016-09-07 20:15:32 +0530

    WA even after reading the editorial

---
+++ abcstr.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<utility> // for pair
+#include<cstring>
+
+#define MAX 1000002
+using namespace std  ; 
+
+
+int main()
+{
+    map<pair<int, int>, int> counter ;
+    int a , b, c, i , j, len; 
+    int ans ; 
+    char str[MAX] ; 
+    a = b = c = 0 ;
+    
+    cin >> str ; 
+    len = strlen(str);
+    for(i = 0  ; i < len ; i++){
+        a += str[i] == 'A' ; 
+        b += str[i] == 'B';
+        c += str[i] == 'C';
+        ans += counter[make_pair(a-b, c-b)] ; 
+        counter[make_pair(a-b, c-b)] ++ ;
+    }
+
+    cout << ans << endl ;
+return 0 ;
+}
+
