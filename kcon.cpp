8e6b068eb439207576de20a63d3c83ac748fd6f1
nickzuck <nikhil1945.singh@gmail.com>
2018-01-15 19:41:43 +0530

    Still WA .. Seriously I need to work on my coding skills

--- kcon.cpp
+++ kcon.cpp
@@ -3,20 +3,46 @@
 
 using namespace std; 
 
+int start_index= 0 , end_index = 0; 
+
 long long maxSubArraySum(vector<int> a, int size)
 {
-     long long max_so_far = INT_MIN, max_ending_here = 0;
-
-    for (int i = 0; i < size; i++)
+    long long max_so_far = INT_MIN, max_ending_here = 0,
+        s=0;
+ 
+    for (int i=0; i< size; i++ )
     {
-        max_ending_here = max_ending_here + a[i];
+        max_ending_here += a[i];
+ 
         if (max_so_far < max_ending_here)
+        {
             max_so_far = max_ending_here;
-
+            start_index = s;
+            end_index = i;
+        }
+ 
         if (max_ending_here < 0)
+        {
             max_ending_here = 0;
+            s = i + 1;
+        }
     }
+    /*
+    cout << "Maximum contiguous sum is "
+        << max_so_far << endl;
+    cout << "Starting index "<< start_index
+        << endl << "Ending index "<< end_index << endl;
+    */
     return max_so_far;
+}
+
+long long sum(vector<int> a, int s_index, int e_index){
+    long long s = 0 ;
+    for(int i = s_index ; i < e_index ; i++){
+        s += a[i]  ;
+    }
+
+    return s ;
 }
 
 int main(){
@@ -39,15 +65,25 @@ int main(){
                 v.pb(v[i]) ;
             }
             max_subarray_sum = maxSubArraySum(v, 2*n) ;
-            //cout << "Max subarray sum " << max_subarray_sum << endl ;
-            if(max_subarray_sum == 2*v_sum){
-                cout << k*v_sum << endl;
+
+            // If max_subarray lies between the single vector
+            // print it as it is
+            if(end_index < n-1){
+                cout <<max_subarray_sum << endl ;
+            }
+            else if(start_index == 0 and end_index == 2*n -1){
+                cout << (k*max_subarray_sum/2) << endl ;
             }
 
             else{
-                cout << max_subarray_sum << endl ;
-            }
+                if(k%2 == 0){
+                    cout << k/2*max_subarray_sum << endl ;
+                }
 
+                else{
+                    cout << k/2 *max_subarray_sum  + sum(v, 0, end_index -n);
+                }
+            }
         }   
         
         else{
