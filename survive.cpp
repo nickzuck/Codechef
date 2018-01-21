82779fce78e7d35e906d1db0cc3b20b3942edbbe
nickzuck <nikhil1945.singh@gmail.com>
2018-01-21 22:45:06 +0530

    Still WA after 5th attempt

--- survive.cpp
+++ survive.cpp
@@ -8,7 +8,7 @@ int main(){
 	cin >> t ;
 	while(t--){
 		cin >> n >> k >> s;
-        int buy = 0, rem = 0, day = 1 ;
+        int buy = 0, rem = 0, day = 1, no_buy_day = 0 ;
         bool flag = true , bought = false;
         for(int i = 0 ; i < s ; i++){
             if(rem < k and day != 7){
@@ -21,39 +21,25 @@ int main(){
                 rem += n ;
                 bought = true ;
             }
-            if(day == 5 and i!= s-2 and rem < 3*k and bought == false){
-                buy ++ ;
-                rem += n ;
-                bought = true ;
-            }
-            if(day == 4 and i!= s-3 and rem < 4*k and bought == false){
-                buy ++ ;
-                rem += n ;
-                bought = true ;
-            }
-            if(day == 3 and i!= s-4 and rem < 5*k and bought == false){
-                buy ++ ;
-                rem += n ;
-                bought = true ;
-            }
-            if(day == 2 and i!= s-5 and rem < 6*k and bought == false){
-                buy ++ ;
-                rem += n ;
-                bought = true ;
-            }
-            if(day == 1 and i!= s-6 and rem < 7*k and bought == false){
-                buy ++ ;
-                rem += n ;
-                bought = true ;
-            }
 
             // deduct chocolates for current day
             //cout << rem << "  " << day << " " << bought << endl ;
-            if(rem < k){
-                flag = false ;
-                break ;
+            if(rem < k ){
+                if(no_buy_day <= 0){
+                    flag = false ;
+                    break ;
+                }
+                else{
+                    bought = true ;
+                    no_buy_day -= 1;
+                    buy ++ ;
+                    rem += n ;
+                }
             }
             rem -= k ;
+            if(bought = false and day != 7){
+                no_buy_day += 1 ;
+            }
             bought = false ;
             if(day == 7){
                 day = 1 ;
