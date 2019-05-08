eadb277be20859e2b149f2afeca48eaaa8789f96
nickzuck_testbook <nikhil.singh@testbook.com>
2019-05-08 09:42:37 +0530

    WA this time

--- redone.cpp
+++ redone.cpp
@@ -9,6 +9,30 @@ ll operate(ll a, ll b){
     return (a%MOD + b%MOD + ((a%MOD) * (b%MOD))%MOD)%MOD;
 }
 
+ll sumOfAllPairs(int n){
+    ll sum = 0 ;
+    for(int i = 1 ; i <= n ; i++){
+        for(int j = i+1 ; j<= n ; j++) {
+            sum = (sum + (i*j)%MOD)%MOD ;
+        }
+    }
+    return sum ; 
+}
+
+ll factorial(int n) {
+    ll fact = 1 ;
+    for(int i = 1 ; i <n ; i++){
+        fact = (fact*i)%MOD ;
+    }
+
+    return fact %MOD;
+}
+
+ll sumToN(int n){
+    return n %2 == 0 ? ((n/2) * (n+1) ) %MOD : ((n+1/2) * (n) ) %MOD;
+}
+
+
 void printArr(int arr[], int n){
     for(int i = 0 ;i < n ; i++){
         cout << arr[i] << endl ;
@@ -26,17 +50,14 @@ int main(){
         } else if (n == 2){
             cout << operate(1, 2) << endl ;
         } else {
-            ll ele = operate(n, n-1);
-            ll i = n-2;
-            // cout << "starting ele : " << ele << endl ;
-            while(i > 0){
-                // cout << "array ele: " << i << endl ;
-                ele = operate(ele, i) ;
-                i --;
-                // cout << "operation result : " << ele  << endl ;
-            }
-            cout << ele << endl ;
-
+            ll result =  sumOfAllPairs(n) ;
+            // cout << "sum of all pairs found " << endl ;
+            result = (result + factorial(n))%MOD ;
+            // cout << "factorial find "<< endl; 
+            result = (result + sumToN(n)) % MOD ; 
+            // cout << "sum to n found " << endl ; 
+            result = result%MOD;
+            cout << result << endl ;
         }
 
     }
