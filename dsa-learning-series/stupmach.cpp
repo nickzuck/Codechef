3353ee7fd0811e267140f1262e3de2fe09cea54c
nickzuck <nikhil1945.singh@gmail.com>
2020-05-07 11:27:28 +0530

    50 points with just long long :(

--- dsa-learning-series/stupmach.cpp
+++ dsa-learning-series/stupmach.cpp
@@ -4,32 +4,35 @@
 using namespace std ;
 
 int main(){
-	int t , n; 
+	long long t , n; 
 	cin >> t ;
 	while (t--){
 		cin >> n ;
-		int temp ; 
-		stack<pair<int, int> > s ;
-		for(int i = 0 ; i < n ; i++){
+		long long temp ; 
+		stack<pair<long long, long long> > s ;
+		for(long long i = 0 ; i < n ; i++){
 			cin >> temp ;
 			if (s.empty()) {
 				s.push(mp(temp, i));
 			} else {
-				pair<int, int> top = s.top() ;
+				pair<long long, long long> top = s.top() ;
 				if (temp < top.first){
 					s.push(mp(temp, i));
 				}
 			}
 		}
 
-		pair<int,int> top = s.top();
-		int subs = top.first ;
-		int ans = top.first * n ;
+		pair<long long,long long> top = s.top();
+		long long subs = top.first ;
+		long long ans = top.first * n ;
 		s.pop() ;
 		while(!s.empty()){
 			top = s.top() ;
-			int val = top.first - subs ;
+			long long val = top.first - subs ;
 			subs += val;
+			if (val <= 0){
+				break;
+			}
 			// cout << "here : " << top.first << "  " << top.second << endl;
 			ans += val * (top.second+1) ;
 			s.pop() ;
