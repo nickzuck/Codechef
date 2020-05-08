5faf8e78459663adea5d97ef5b50d0b1baa04993
nickzuck <nikhil1945.singh@gmail.com>
2020-05-08 12:00:12 +0530

    Finally AC, was missing the case <><

--- dsa-learning-series/compiler.py
+++ dsa-learning-series/compiler.py
@@ -1,18 +1,24 @@
 t = int(input())
 for j in range(t):
 	s = input()
-	stack = []
-	add = 0
-	for i in range(len(s)) :
-		if s[i] == "<" :
-			stack.append(s[i])
-		else :
-			if len(stack) == 0 :
-				break 
-			stack.pop()
-			if len(stack) == 0 :
-				add = i
-	if len(stack) != 0:
-		print (0)		
+
+	counter = 0 
+	prefix = 0
+	errorFound = False
+	for i in range(len(s)):
+		if s[i] == ">":
+			if counter == 0 :
+				errorFound = True
+				break
+			else :
+				counter -= 1
+				if counter == 0 :
+					prefix = i
+
+		if s[i] == '<':
+			counter +=1
+
+	if (counter == 0 or(counter !=0 and errorFound == False) )and prefix != 0:
+		print (prefix + 1)
 	else :
-		print (add +1)
+		print (0)
