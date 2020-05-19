8172b9359444b51d21e11b7d0b942aee841376b1
nickzuck <nikhil1945.singh@gmail.com>
2020-05-19 09:57:36 +0530

    Still getting partial AC.. and TLE on rest of the cases

--- dsa-learning-series/dpairs.cpp
+++ dsa-learning-series/dpairs.cpp
@@ -7,17 +7,20 @@ int main(){
 	int temp , n, m; 
 	cin >> n >> m;
 	vector<int> v1, v2 ;
+	unordered_set<int> s1, s2;
 	for (int i = 0 ;i < n; i++){
 		cin >> temp ;
-		v1.push_back(temp);
+		if (s1.find(temp) == s1.end())
+			v1.push_back(temp);
 	}
 	for (int j = 0 ; j < m ; j++){
 		cin >> temp ;
-		v2.push_back(temp);
+		if (s2.find(temp) == s2.end())
+			v2.push_back(temp);
 	}
 	unordered_set <int> s ;
 	int count = 0 ;
-	for(int i = 0 ;i <n ;i++){
+	for(int i = 0 ;i < n ;i++){
 		for(int j= 0 ; j < m ; j++){
 			// cout << "sum : " << v1[i] + v2[j] << endl ;
 			if (s.find(v1[i] + v2[j]) == s.end() && count < n+m-1){
