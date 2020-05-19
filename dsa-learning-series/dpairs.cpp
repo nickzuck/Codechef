5a14a3f0f7608122848352b7388cc88a6cd95693
nickzuck <nikhil1945.singh@gmail.com>
2020-05-19 10:39:38 +0530

    TLE solved on some subproblems, but still there

--- dsa-learning-series/dpairs.cpp
+++ dsa-learning-series/dpairs.cpp
@@ -6,18 +6,19 @@ using namespace std ;
 int main(){
 	int temp , n, m; 
 	cin >> n >> m;
-	vector<int> v1, v2 ;
+	vector<pair<int, int> > v1, v2 ;
 	// unordered_set<int> s1, s2;
 	for (int i = 0 ;i < n; i++){
 		cin >> temp ;
 		// if (s1.find(temp) == s1.end())
-		v1.push_back(temp);
+		v1.push_back(make_pair(temp, i));
 	}
 	for (int j = 0 ; j < m ; j++){
 		cin >> temp ;
 		// if (s2.find(temp) == s2.end())
-		v2.push_back(temp);
+		v2.push_back(make_pair(temp, j));
 	}
+
 	unordered_set <int> s ;
 	int count = 0 ;
 	sort(v1.begin(), v1.end());
@@ -28,10 +29,10 @@ int main(){
 			if (count == n+m-1) {
 				break;
 			}
-			if (s.find(v1[i] + v2[j]) == s.end() && count < n+m-1){
+			if (s.find(v1[i].first + v2[j].first) == s.end() && count < n+m-1){
 				count += 1 ;
-				cout << i  << " "  << j << endl ;
-				s.insert(v1[i] + v2[j]);
+				cout << v1[i].second  << " "  << v2[j].second << endl ;
+				s.insert(v1[i].first + v2[j].first);
 			}
 		}
 	}
