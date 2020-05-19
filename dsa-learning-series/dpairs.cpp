9c09d850f15e5b492ed73f8969de2ff16716a5b2
nickzuck <nikhil1945.singh@gmail.com>
2020-05-19 10:34:29 +0530

    wrong answer on some cases, due to change in index after sorting

--- dsa-learning-series/dpairs.cpp
+++ dsa-learning-series/dpairs.cpp
@@ -7,22 +7,27 @@ int main(){
 	int temp , n, m; 
 	cin >> n >> m;
 	vector<int> v1, v2 ;
-	unordered_set<int> s1, s2;
+	// unordered_set<int> s1, s2;
 	for (int i = 0 ;i < n; i++){
 		cin >> temp ;
-		if (s1.find(temp) == s1.end())
-			v1.push_back(temp);
+		// if (s1.find(temp) == s1.end())
+		v1.push_back(temp);
 	}
 	for (int j = 0 ; j < m ; j++){
 		cin >> temp ;
-		if (s2.find(temp) == s2.end())
-			v2.push_back(temp);
+		// if (s2.find(temp) == s2.end())
+		v2.push_back(temp);
 	}
 	unordered_set <int> s ;
 	int count = 0 ;
+	sort(v1.begin(), v1.end());
+	sort(v2.begin(), v2.end());
 	for(int i = 0 ;i < n ;i++){
-		for(int j= 0 ; j < m ; j++){
+		for(int j= m-1 ; j >= 0; j--){
 			// cout << "sum : " << v1[i] + v2[j] << endl ;
+			if (count == n+m-1) {
+				break;
+			}
 			if (s.find(v1[i] + v2[j]) == s.end() && count < n+m-1){
 				count += 1 ;
 				cout << i  << " "  << j << endl ;
