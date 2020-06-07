b550166ee142a8d6f28e8f282baa8a9d9b061ea3
nickzuck <nikhil1945.singh@gmail.com>
2020-06-07 07:13:40 +0530

    Partially AC.. Was getting WA coz of range of values

--- dsa-learning-series/ashigift.cpp
+++ dsa-learning-series/ashigift.cpp
@@ -1,37 +1,40 @@
 #include<bits/stdc++.h>
 
-#define pr pair<int, int>
+#define pr pair<long long, long long>
 #define pb push_back
 #define mp make_pair
 
 using namespace std ;
 
 
-bool canCross(int intial, vector<pr> &v){
-	for(int i = 0 ; i < v.size(); i++){
+bool canCross(long long intial, vector<pr> &v){
+	for(long long i = 0 ; i < v.size(); i++){
 		intial += v[i].second ;
+		if(intial < 0){
+			return false ;
+		}
 	}
 	return intial > 0 ;
 }
 
 int main(){
-	int t , x, b, c; 
-	int temp1, temp2 , temp3;
+	long long t , x, b, c; 
+	long long temp1, temp2 , temp3;
 	cin >> t ;
 	while (t--){
 		cin >> x ;
 		cin >> b ;
 
 		vector<pr> v ;
-		int max_poison = 0 ;
-		for (int i = 0 ;i < b; i++){
+		long long max_poison = 0 ;
+		for (long long i = 0 ;i < b; i++){
 			cin >> temp1 >> temp2 ;
 			v.pb(mp(temp1, -temp2)) ;
 			max_poison += temp2 ;
 		}
 
 		cin >> c ;
-		for(int i = 0 ;i < c ; i++){
+		for(long long i = 0 ;i < c ; i++){
 			// cout << "input here\n" ; 
 			cin >> temp1 >> temp2 >> temp3 ;
 			temp2 = min(temp2, temp3);
@@ -39,15 +42,15 @@ int main(){
 		}
 
 		sort(v.begin(), v.end());
-		int count = 0 ;
-		for(int i= 0 ;i < v.size() ; i++){
+		long long count = 0 ;
+		for(long long i= 0 ;i < v.size() ; i++){
 			count += v[i].second ;
 		}
 
-		int start = 1 , end = max_poison, ans = max_poison;
+		long long start = 1 , end = max_poison, ans = max_poison;
 		while(start <= end){
 			// cout << "processing \n" ;
-			int mid = (start +end)/2 ;
+			long long mid = (start +end)/2 ;
 			if(canCross(mid, v)){
 				ans = min(ans, mid);
 				end = mid -1 ;
