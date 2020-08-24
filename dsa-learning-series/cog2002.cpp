d22d41a6fcb25ba1be5b6e9c5a28a7d03aba7172
nickzuck <nikhil1945.singh@gmail.com>
2020-08-24 18:37:02 +0530

    WA

---
+++ dsa-learning-series/cog2002.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+#define pr pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define ll long long 
+
+using namespace std ;
+
+int main(){
+	int t , n, temp; 
+	cin >> t ;
+	while (t--){
+		cin >> n ;
+		// cout << "n = " << n << endl ;
+		vector<int> v ; 
+		for(int i = 0 ; i < n ;i++){
+			cin >> temp ;
+			v.pb(temp);
+		}
+
+		ll ans = LLONG_MIN, sum  ;
+		// cout << "here\n" ;
+		for(int i = 0 ;i < n ; i++){
+
+			ll s1, s2 ;
+			if (i == 0 ){
+				s1 = v[i] + v[n-1] + v[n-2] ;
+				s2 = v[i] + v[n-1] + v[i+1] ;
+			} else if (i == 1){
+				s1 = v[0] + v[i] + v[n-1] ;
+				s2 = v[i] + v[i-1] + v[(i+1)%n] ;
+			} else {
+				s1 = v[i] + v[i-1] + v[i-2] ;
+				s2 = v[i] + v[i-1] + v[(i+1) %n] ;
+			}
+			
+			ll s3 = v[i] + v[ (i+1) % n] + v[(i+2) %n] ;
+			// cout << s1 << " "  << s2 << " " << s3 << endl ;
+			sum = max(s1, max(s2, s3));
+			ans = max(ans, sum);
+		}
+		cout << ans << endl ;
+
+	}
+	return 0;
+}
