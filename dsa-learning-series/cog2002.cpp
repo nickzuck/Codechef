ad5543e96c61e1fc95dff26ef1aad9553393507a
nickzuck <nikhil1945.singh@gmail.com>
2020-08-24 18:56:48 +0530

    Still WA

--- dsa-learning-series/cog2002.cpp
+++ dsa-learning-series/cog2002.cpp
@@ -23,24 +23,27 @@ int main(){
 		// cout << "here\n" ;
 		for(int i = 0 ;i < n ; i++){
 
-			ll s1, s2, s4 ;
+			ll s1, s2, s4 , s5;
 			if (i == 0 ){
 				s1 = v[i] + v[n-1] + v[n-2] ;
 				s2 = v[i] + v[n-1] + v[i+1] ;
 				s4 = v[i] + v[n-2] + v[i+2] ;
+				s5 = v[i] + v[i+1] + v[n-2]; 
 			} else if (i == 1){
 				s1 = v[0] + v[i] + v[n-1] ;
 				s2 = v[i] + v[i-1] + v[(i+1)%n] ;
 				s4 = v[i] + v[n-1] + v[(i+2)%n] ;
+				s5 = v[i] + v[i+1] + v[n-1] ;
 			} else {
 				s1 = v[i] + v[i-1] + v[i-2] ;
 				s2 = v[i] + v[i-1] + v[(i+1) %n] ;
 				s4 = v[i] + v[i-2] + v[(i+2)%n] ;
+				s5 = v[i] + v[(i+1)%n] + v[i-2];
 			}
 			
 			ll s3 = v[i] + v[ (i+1) % n] + v[(i+2) %n] ;
 			// cout << s1 << " "  << s2 << " " << s3 << " " << s4 << endl  ;
-			sum = max(max(s1, max(s2, s3)), s4);
+			sum = max(s5, max(max(s1, max(s2, s3)), s4));
 			ans = max(ans, sum);
 		}
 		cout << ans << endl ;
