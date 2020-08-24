7ea44bf6b208d9076d2f0d617696138f21dc7e8f
nickzuck <nikhil1945.singh@gmail.com>
2020-08-24 18:48:48 +0530

    Still WA.. That's easy we can do it

--- dsa-learning-series/cog2002.cpp
+++ dsa-learning-series/cog2002.cpp
@@ -23,21 +23,24 @@ int main(){
 		// cout << "here\n" ;
 		for(int i = 0 ;i < n ; i++){
 
-			ll s1, s2 ;
+			ll s1, s2, s4 ;
 			if (i == 0 ){
 				s1 = v[i] + v[n-1] + v[n-2] ;
 				s2 = v[i] + v[n-1] + v[i+1] ;
+				s4 = v[i] + v[n-2] + v[i+2] ;
 			} else if (i == 1){
 				s1 = v[0] + v[i] + v[n-1] ;
 				s2 = v[i] + v[i-1] + v[(i+1)%n] ;
+				s4 = v[i] + v[n-1] + v[(i+2)%n] ;
 			} else {
 				s1 = v[i] + v[i-1] + v[i-2] ;
 				s2 = v[i] + v[i-1] + v[(i+1) %n] ;
+				s4 = v[i] + v[i-2] + v[(i+2)%n] ;
 			}
 			
 			ll s3 = v[i] + v[ (i+1) % n] + v[(i+2) %n] ;
-			// cout << s1 << " "  << s2 << " " << s3 << endl ;
-			sum = max(s1, max(s2, s3));
+			// cout << s1 << " "  << s2 << " " << s3 << " " << s4 << endl  ;
+			sum = max(max(s1, max(s2, s3)), s4);
 			ans = max(ans, sum);
 		}
 		cout << ans << endl ;
