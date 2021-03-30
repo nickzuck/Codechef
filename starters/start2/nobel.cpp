8fda67d0ab8ac4d4e9c10b9615abd40bcdcb87eb
nickzuck <nikhil1945.singh@gmail.com>
2021-03-30 10:51:53 +0530

    3TLEs and 1 WA... this shouldn't be that tough

--- starters/start2/nobel.cpp
+++ starters/start2/nobel.cpp
@@ -9,27 +9,36 @@ int main(){
 		cin >> n >> m ;
 
 		map<int,bool> mp ;
-		// for(int i = 1 ; i <= m ; i++){
-		// 	mp[i] = true;
+		// int xorVal = m ;
+		// for(int i = 1 ; i <= m-1 ; i++){
+		// 	xorVal ^= i ;
 		// }
 
 		int temp ;
+
+		// cout << "xorVal = "<< xorVal << endl ;
+		int distinct = 0 ;
 		for(int i = 0 ; i<n; i++){
 			cin >> temp ;
-			mp[temp] = true ;
-		}
-
-		bool found = false ;
-		for(int i = 1 ; i < m ; i++){
-			if (mp[i]  == false){
-				found = true ;
-				break ;
+			if (!mp[temp]){
+				mp[temp] = true ;
+				// xorVal ^= temp ;
+				distinct ++ ;
 			}
 		}
-		if (found){
-			cout << "Yes\n";
-		} else {
+		// cout << "xorVal -->  " << xorVal << endl ;
+
+		// bool found = false ;
+		// for(int i = 1 ; i < m ; i++){
+		// 	if (mp[i]  == false){
+		// 		found = true ;
+		// 		break ;
+		// 	}
+		// }
+		if (distinct == m){
 			cout << "No\n";
+		} else {
+			cout << "Yes\n";
 		}
 	}
 	return 0 ;
